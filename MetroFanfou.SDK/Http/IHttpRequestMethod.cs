01d7aa0e1cfdf8bcc9afffcdc436c72967eeee96
dodola <dinophp@gmail.com>
2012-07-19 22:43:21 +0800

    删除旧版SDK

--- MetroFanfou.SDK/Http/IHttpRequestMethod.cs
+++
@@ -1,7 +0,0 @@
-namespace MetroFanfou.SDK.Http
-{
-    public interface IHttpRequestMethod
-    {
-        string Request(string uri, string postData);
-    }
-}
--- MetroFanfou.SDK/Objects/NameValuePairCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace MetroFanfou.SDK.Objects
-{
-    public class NameValuePairCollection : Dictionary<string, string>
-    {
-        
-    }
-}
